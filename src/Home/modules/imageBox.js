4efd86d357dc09870f46e6bc9e0790d9d04dab67
subhanshup <subhanshu0102@gmail.com>
2021-02-14 13:55:45 +0530

    Added Lazy Load Component for Images

--- src/Home/modules/imageBox.js
+++ src/Home/modules/imageBox.js
@@ -1,10 +1,18 @@
 
 import React from "react"
 import Heading from '../modules/heading'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 export default function ImageBox({title="",href={},image="",srcSet="",className=""}){
     return(
         <div className={"imagebox-container " + className}>
-            {image && <img src={image} srcSet={srcSet} alt={title}/>}
+            {image &&
+                <LazyLoadImage
+                 src={image} 
+                 srcSet={srcSet} 
+                 alt={title}
+                />
+            }
             <div className="imagebox-desc">
                 <Heading className="imagebox-title" tag="h3" content={title}/>
                 {href.link && <a className="imagebox-a" href={href.link}>{href.title}</a>}
